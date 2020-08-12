2ea279e8e5275dccc16cd00cc7735e3402377352
Zeprit <45455849+Zeprit@users.noreply.github.com>
2020-08-12 22:25:41 +0200

    Update client.js

--- public/client.js
+++ public/client.js
@@ -27,8 +27,8 @@ var SOUND = true;
 var AFK = false;
 
 //native canvas resolution
-var NATIVE_WIDTH = 256; //128
-var NATIVE_HEIGHT = 200;
+var NATIVE_WIDTH = 128; //128
+var NATIVE_HEIGHT = 100;
 
 /*
 The original resolution (pre canvas stretch) is 128x100 multiplied by 2 because
