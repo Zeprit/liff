b3af80882bc38d6842c5047453b5dc52b10a2164
Carcassii <48177997+Carcassii@users.noreply.github.com>
2020-08-27 20:35:01 +0200

    Update client.js

--- public/client.js
+++ public/client.js
@@ -1561,13 +1561,13 @@ function avatarSelection() {
     nextBody.animation.stop();
     menuGroup.add(nextBody);
 
-    previousColor = createSprite(178 * ASSET_SCALE + 14, 50 * ASSET_SCALE + 14);
+    previousColor = createSprite(177 * ASSET_SCALE + 14, 50 * ASSET_SCALE + 14);
     previousColor.addAnimation("default", animation);
     previousColor.animation.stop();
     previousColor.mirrorX(-1);
     menuGroup.add(previousColor);
 
-    nextColor = createSprite(194 * ASSET_SCALE + 14, 50 * ASSET_SCALE + 14);
+    nextColor = createSprite(193 * ASSET_SCALE + 14, 50 * ASSET_SCALE + 14);
     nextColor.addAnimation("default", animation);
     nextColor.animation.stop();
     menuGroup.add(nextColor);
