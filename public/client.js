6077f23cc91e183fec3b0d1ac20856f583acb6aa
Carcassii <48177997+Carcassii@users.noreply.github.com>
2020-08-27 16:42:10 +0200

    Update client.js

--- public/client.js
+++ public/client.js
@@ -1148,7 +1148,7 @@ function update() {
         textFont(font, FONT_SIZE * 2);
         textAlign(CENTER, BASELINE);
         fill(0);
-        text("Body", 24 * ASSET_SCALE, 44 * ASSET_SCALE);
+        text("Body", WIDTH / 4, 44 * ASSET_SCALE);
         text("Color", (WIDTH) - (WIDTH / 4), 44 * ASSET_SCALE);
 
         text("Choose your avatar", WIDTH / 2, 18 * ASSET_SCALE);
